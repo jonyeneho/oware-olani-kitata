0f769f34b5ae258291b514d5c0b97fa24693e7bf
Jide Onyeneho <jeo2006@netscape.net>
2017-04-29 15:27:37 -0500

    added storehouse test and added all tests

---
+++ test/AllOwareTestsJide.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BoardTest.class, MoveTest.class, PitTest.class,
+    TestStorehouseJide.class})
+
+	public class AllOwareTestsJide {
+	
+	}
---
+++ test/TestStorehouseJide.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStorehouseJide {
+
+	@Test
+	public void test() {
+		Storehouse s = new Storehouse();
+		
+		assertEquals(0, s.getNumSeeds());
+	}
+
+}
